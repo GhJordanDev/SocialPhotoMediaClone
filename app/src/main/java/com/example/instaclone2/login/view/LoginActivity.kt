d6decab26ba96555645f50889acbdcebe4dd5941
GhJordanDev <jordangitdevaccount@proton.me>
2025-06-20 12:30:52 -0300

    Add Form validation TextInputLayout

--- app/src/main/java/com/example/instaclone2/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/instaclone2/login/view/LoginActivity.kt
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.instaclone2.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextInputLayout>(R.id.login_edit_email_input).error = "E-mail inválido"
 
     }
 }
