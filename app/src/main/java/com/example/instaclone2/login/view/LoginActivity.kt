76e410ccab102a36c82ccb1fb36ae4ea322caafd
GhJordanDev <jordangitdevaccount@proton.me>
2025-06-22 19:58:30 -0300

    Add TextWatcher

--- app/src/main/java/com/example/instaclone2/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/instaclone2/login/view/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.instaclone2.login.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.instaclone2.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
@@ -13,7 +17,31 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        findViewById<TextInputLayout>(R.id.login_edit_email_input).error = "E-mail inválido"
+        val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+        val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_password)
+
+        editTextEmail.addTextChangedListener(watcher)
+        editTextPassword.addTextChangedListener(watcher)
+
+        findViewById<Button>(R.id.login_btn_enter).setOnClickListener {
+            findViewById<TextInputLayout>(R.id.login_edit_email_input).error = "E-mail inválido"
+
+            findViewById<TextInputLayout>(R.id.login_edit_password_input).error = "Senha incorrta"
+        }
+    }
+
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            findViewById<Button>(R.id.login_btn_enter).isEnabled = p0.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+            TODO("Not yet implemented")
+        }
 
     }
 }
