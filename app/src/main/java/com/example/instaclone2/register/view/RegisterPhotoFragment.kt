92aa29eb565d9040d254a743e1fc5d69a9e9f679
GhJordanDev <jordangitdevaccount@proton.me>
2025-06-26 12:30:51 -0300

    Add Custom Dialog

--- app/src/main/java/com/example/instaclone2/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/com/example/instaclone2/register/view/RegisterPhotoFragment.kt
@@ -1,11 +1,13 @@
 package com.example.instaclone2.register.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.instaclone2.R
+import com.example.instaclone2.common.view.CustomDialog
 
 class RegisterPhotoFragment : Fragment() {
 
@@ -15,5 +17,27 @@ class RegisterPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+
+        customDialog.setTitle(R.string.app_name)
+
+        customDialog.addButton(R.string.photo, R.string.gallery){
+            when(it.id){
+                R.string.photo -> {
+                    Log.i("Teste", "Foto")
+                }
+                R.string.gallery -> {
+                    Log.i("Teste", "Galeria")
+                }
+            }
+        }
+
+
+        customDialog.show()
     }
 }
