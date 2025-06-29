7b19dca65800e377c80990f51f95e11b473a0260
GhJordanDev <jordangitdevaccount@proton.me>
2025-06-29 18:21:28 -0300

    Add profile user layout

---
+++ app/src/main/java/com/example/instaclone2/profile/view/FragmentProfile.kt
@@ -0,0 +1,19 @@
+package com.example.instaclone2.profile.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.instaclone2.R
+
+class FragmentProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
