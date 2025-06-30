00a5d0ece2805c21003bb4a8ada0c17ce8ac8fa9
GhJordanDev <jordangitdevaccount@proton.me>
2025-06-30 11:37:39 -0300

    Changing MainActivity StatusBarColor

--- app/src/main/java/com/example/instaclone2/main/view/MainActivity.kt
+++ app/src/main/java/com/example/instaclone2/main/view/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.instaclone2.main.view
 
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.instaclone2.R
@@ -10,12 +12,13 @@ import com.example.instaclone2.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        window.insetsController?.setSystemBarsAppearance(
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+        )
+        window.statusBarColor = ContextCompat.getColor(this, R.color.grey)
+
     }
 }
