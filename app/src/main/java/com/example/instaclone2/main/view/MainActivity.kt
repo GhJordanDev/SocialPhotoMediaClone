535a88b30f7bc7c803a0d042c4a3742c7399a083
GhJordanDev <jordangitdevaccount@proton.me>
2025-07-20 21:31:37 -0300

    Method Load All Fragments  MainActivity

--- app/src/main/java/com/example/instaclone2/main/view/MainActivity.kt
+++ app/src/main/java/com/example/instaclone2/main/view/MainActivity.kt
@@ -24,21 +24,21 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var homeFragment: Fragment
     private lateinit var searchFragment: Fragment
     private lateinit var cameraFragment: Fragment
     private lateinit var profileFragment: Fragment
 
-    private var currentFragment: Fragment? = null
+    private lateinit var currentFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.setSystemBarsAppearance(
                 WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
                 WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
@@ -57,18 +57,29 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         cameraFragment = CameraFragment()
         profileFragment = ProfileFragment()
 
-        binding.mainBottomNav.setOnNavigationItemSelectedListener (this)
-        binding.mainBottomNav.selectedItemId = R.id.menu_bottom_home
+        currentFragment = homeFragment
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.main_fragment, profileFragment, "3").hide(profileFragment)
+            add(R.id.main_fragment, cameraFragment, "2").hide(cameraFragment)
+            add(R.id.main_fragment, searchFragment, "1").hide(searchFragment)
+            add(R.id.main_fragment, homeFragment, "0")
+            commit()
+        }
+
+
+        binding.mainBottomNav.setOnNavigationItemSelectedListener(this)
+        //binding.mainBottomNav.selectedItemId = R.id.menu_bottom_home
     }
 
-    private fun setScrollToolbarEnabled (enabled: Boolean){
+    private fun setScrollToolbarEnabled(enabled: Boolean) {
         val params = binding.mainToolbar.layoutParams as AppBarLayout.LayoutParams
         val coordinatorParams = binding.mainAppbar.layoutParams as CoordinatorLayout.LayoutParams
 
-        if(enabled){
-            params.scrollFlags = AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL or AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
+        if (enabled) {
+            params.scrollFlags =
+                AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL or AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
             coordinatorParams.behavior = AppBarLayout.Behavior()
-        }else {
+        } else {
             params.scrollFlags = 0
             coordinatorParams.behavior = null
         }
@@ -77,31 +88,38 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         var scrollToolbarEnabled = false
-        when(item.itemId){
+        when (item.itemId) {
             R.id.menu_bottom_home -> {
-                if(currentFragment == homeFragment) return false
+                if (currentFragment == homeFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(homeFragment).commit()
                 currentFragment = homeFragment
             }
+
             R.id.menu_bottom_search -> {
-                if(currentFragment == searchFragment) return false
+                if (currentFragment == searchFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(searchFragment).commit()
                 currentFragment = searchFragment
             }
+
             R.id.menu_bottom_add -> {
-                if(currentFragment == cameraFragment) return false
+                if (currentFragment == cameraFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(cameraFragment).commit()
                 currentFragment = cameraFragment
             }
+
             R.id.menu_bottom_profile -> {
-                if(currentFragment == profileFragment) return false
+                if (currentFragment == profileFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(profileFragment).commit()
                 currentFragment = profileFragment
                 scrollToolbarEnabled = true
             }
         }
 
-        setScrollToolbarEnabled (scrollToolbarEnabled)
+        setScrollToolbarEnabled(scrollToolbarEnabled)
 
-        currentFragment?.let{
+        /*currentFragment?.let{
             replaceFragment(R.id.main_fragment, it)
-        }
+        }*/
         return true
     }
 }
