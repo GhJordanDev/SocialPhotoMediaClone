e637c999f99d139f2bb19fd251b2c5b8a623596f
GhJordanDev <jordangitdevaccount@proton.me>
2025-07-29 10:34:48 -0300

    Search View Case | Toolbar Search

---
+++ app/src/main/java/com/example/instaclone2/search/Search.kt
@@ -0,0 +1,15 @@
+package com.example.instaclone2.search
+
+import com.example.instaclone2.common.base.BasePresenter
+import com.example.instaclone2.common.base.BaseView
+
+interface Search {
+
+    interface Presenter : BasePresenter {
+        
+    }
+
+    interface View : BaseView<Presenter>{
+
+    }
+}
--- app/src/main/java/com/example/instaclone2/search/view/SearchFragment.kt
+++ app/src/main/java/com/example/instaclone2/search/view/SearchFragment.kt
@@ -1,5 +1,7 @@
 package com.example.instaclone2.search.view
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -7,37 +9,51 @@ import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instaclone2.R
+import com.example.instaclone2.common.base.BaseFragment
+import com.example.instaclone2.databinding.FragmentSearchBinding
+import com.example.instaclone2.search.Search
 
-class SearchFragment : Fragment() {
+class SearchFragment : BaseFragment<FragmentSearchBinding, Search.Presenter>(
+    R.layout.fragment_search,
+    FragmentSearchBinding::bind
+) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    override lateinit var presenter: Search.Presenter
+
+    override fun setupViews() {
+        binding?.searchRv?.layoutManager = LinearLayoutManager(requireContext())
+        binding?.searchRv?.adapter = PostAdapter()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val rv = view.findViewById<RecyclerView>(R.id.search_rv)
-        rv.layoutManager = LinearLayoutManager(requireContext())
-        rv.adapter = PostAdapter()
+    override fun setupPresenter() {
+        TODO("Not yet implemented")
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
+    override fun getMenu() = R.menu.menu_search
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_profile, menu)
         super.onCreateOptionsMenu(menu, inflater)
+
+        val searchManager = requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = (menu.findItem(R.id.menu_search).actionView as SearchView)
+        searchView.apply {
+            setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(p0: String?): Boolean {
+                    return false
+                }
+            })
+        }
+
     }
 
     private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>(){
